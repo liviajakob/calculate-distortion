d5c239439bfd4b75450db7f095ef50e2171fe9a5
Livia <livia.jakob@gmx.ch>
2019-03-22 10:24:41 +0000

    initial commit

---
+++ distortion-polar-north.py
@@ -0,0 +1,46 @@
+from geopy.distance import geodesic
+from geopy.distance import great_circle
+from pyproj import Proj, transform
+import math
+
+
+## latLon EPSG: 4326
+## polarstereo north EPSG: 3413
+## polarstereo antarctic EPSG: 3031
+
+## point 1
+lat1 = 80.0
+lon1 = 1.0
+
+## point 2
+lat2 = 80.0
+lon2 = 2.0
+
+
+## project
+inProj = Proj(init='epsg:4326')
+outProj = Proj(init='epsg:3413')
+pLon1, pLat1 = transform(inProj, outProj, lon1, lat1)
+pLon2, pLat2 = transform(inProj, outProj, lon2, lat2)
+print("Projected point 1: ", pLat1, pLon1)
+print("Projected point 2: ", pLat2, pLon2)
+distProj = math.sqrt(((pLat1-pLat2)*(pLat1-pLat2)) + ((pLon1-pLon2)*(pLon1-pLon2)))/1000
+print('Projected Distance: {} km'.format(distProj))
+
+## geodetic
+loc1 = (lat1, lon1)
+loc2 = (lat2, lon2)
+distGeo = geodesic(loc1, loc2).kilometers
+print("Geodetic Distance: {} km".format(distGeo))
+
+## using great circle distance
+loc1 = (lat1, lon1)
+loc2 = (lat2, lon2)
+distGC = great_circle(loc1, loc2).kilometers
+print("Great Circle Distance: {} km".format(distGC))
+
+print("--")
+print("Distortion from Geodetic and Great Circle Distance:")
+print("Distortion Geodetic: {} %".format((1-distGeo/distProj)*100))
+print("Distortion Great Circle: {} %".format((1-distGC/distProj)*100))
+print("Distortion GC for 1 km: {} km".format((distGC-distProj)/distGC))
---
+++ distortion-steps-lat.py
@@ -0,0 +1,48 @@
+'''
+Calculates distortion in distance of a projection by stepping through different latitudes
+'''
+from geopy.distance import great_circle
+from pyproj import Proj, transform
+import math
+
+
+
+
+def distortion(lat1, lon1, lat2, lon2, epsg):
+    inProj = Proj(init='epsg:4326')
+
+    ### for EPSG code projection
+    # epsg='epsg:'+epsg
+    # outProj = Proj(init=epsg)
+
+    ### for dynamic projection
+    outProj = Proj(epsg.format(lat1))
+
+    pLon1, pLat1 = transform(inProj, outProj, lon1, lat1)
+    pLon2, pLat2 = transform(inProj, outProj, lon2, lat2)
+
+    ### calculate projected distance
+    distProj = math.sqrt(((pLat1-pLat2)*(pLat1-pLat2)) + ((pLon1-pLon2)*(pLon1-pLon2)))/1000
+
+    ## using great circle distance
+    loc1 = (lat1, lon1)
+    loc2 = (lat2, lon2)
+    distGC = great_circle(loc1, loc2).kilometers
+
+    ### print distortion of projected distance
+    print("{} \t {} %".format(lat1, abs(1-distGC/distProj)*100))
+
+
+if __name__== "__main__":
+    # define latitudes, longitude and projection string
+    lon1 = 31.0
+    lon2 = 42.0
+    lat = -85.0
+    epsg = "+proj=stere +lat_0=90 +lat_ts={} +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"
+
+    # step through latitudes
+    while lat<90.0:
+        if lat != 0.0:
+            distortion(lat, lon1, lat, lon2, epsg)
+        lat = lat+5.0
+
